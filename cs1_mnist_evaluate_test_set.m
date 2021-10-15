54420bcd7cb8d0dd3308da4811507044068efeed
quankhuc <quankhuc@wustl.edu>
2021-10-15 13:11:15 -0500

    commit

--- cs1_mnist_evaluate_test_set.m
+++ cs1_mnist_evaluate_test_set.m
@@ -11,6 +11,7 @@
 % IMPORTANT!!:
 % You should save 1) and 2) in a file named 'classifierdata.mat' as part of
 % your submission.
+% TODO: NEED TO HAVE centroid_labels
 
 predictions = zeros(200,1);
 outliers = zeros(200,1);
@@ -30,13 +31,63 @@ end
 %% DESIGN AND IMPLEMENT A STRATEGY TO SET THE outliers VECTOR
 % outliers(i) should be set to 1 if the i^th entry is an outlier
 % otherwise, outliers(i) should be 0
-% FILL IN
-% I use the function isoutlier() given by MATLAB. In this function, I use a
-% parameter TODO: Fill this in
+sum_all_rows = sum(train(:,1:784));
+outliers_filter = isoutlier(sum_all_rows);
+inliers_filter = ~outliers_filter;
+
+not_match_count_rows = zeros(size(test,1),1);
+
+for i=1:200
+    not_match_count_row = 0;
+    for j=1:784
+        if(outliers_filter(j) == 0 && test(i,j) ~= 0)
+            not_match_count_row = not_match_count_row + 1;
+        end
+    end
+%     Convert this array to row major order to increase efficency of cache
+    not_match_count_rows(i) = not_match_count_row;
+end
+
+% Uncommemt this to use the method that classify outlier to have the
+% mismatch count to be bigger than 95% of all elements in
+% "not_match_count_rows" array
+% quantile_stats = quantile(not_match_count_rows,0.95);
+% count_of_outliers = sum(not_match_count_rows > quantile_stats);
+% % colNrs is an array contains indices of predicted outliers
+% colNrs = find(not_match_count_rows >= quantile_stats);
+
+
+% We used isoutlier function given by matlab to detect outliers within the
+% "not_match_count_rows" array
+outliers_ = isoutlier(not_match_count_rows);
+% colNrs is an array contains indices of predicted outliers
+colNrs = find(outliers_ > 0);
+count_of_outliers = length(colNrs);
+
+figure;
+colormap('gray');
+
+plotsize_outliers = ceil(sqrt(length(colNrs)));
+
+for ind=1:length(colNrs)
+    
+    outlier=test(colNrs(ind),(1:784));
+    subplot(plotsize,plotsize,ind);
+    
+    imagesc(reshape(outlier,[28 28])');
+    title(strcat('Outlier',num2str(ind)))
+
+end
+
+% Identify outliers to be image 3 (image 56th) and image 9 (image 187th). 
+% Other images are doubtfully to be outliers. Surprisingly, the two methods
+% confirmed that image 9 are outliers
     
 %% MAKE A STEM PLOT OF THE OUTLIER FLAG
 figure;
 % FILL IN
+stem(outliers_);
+title("Outlier stem plot");
 
 %% The following plots the correct and incorrect predictions
 % Make sure you understand how this plot is constructed
